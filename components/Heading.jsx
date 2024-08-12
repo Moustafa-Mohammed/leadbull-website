5a25a0ea00993874061dd442f416e6850eb29d6d
Mostafa Mohamed <thedevelopermostafa@gmail.com>
2024-08-12 18:48:06 +0300

    fix: Added size prop to Heading

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -5,6 +5,7 @@ const Heading = ({
   level = 1,
   children,
   color = 'text-white',
+  size = 'sm',
   className = '',
 }) => {
   const Tag = `h${level}`;
@@ -14,10 +15,19 @@ const Heading = ({
     gradient:
       'bg-clip-text text-transparent bg-gradient-to-br from-[#FF0000] via-[#027FCA] to-[#027FCA] ',
   };
+  const sizesStyles = {
+    sm: ' text-lg md:text-xl lg:text-2xl',
+    lg: ' text-xl md:text-3xl lg:text-4xl',
+  };
 
-  const baseStyles = 'text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold';
+  const baseStyles = 'font-bold';
 
-  const combinedStyles = classNames(baseStyles, colorStyles[color], className);
+  const combinedStyles = classNames(
+    baseStyles,
+    colorStyles[color],
+    sizesStyles[size],
+    className
+  );
 
   return <Tag className={combinedStyles}>{children}</Tag>;
 };
