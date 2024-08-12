73fefac978962d87c200ab8507b8da8c538988c8
Mostafa Mohamed <thedevelopermostafa@gmail.com>
2024-08-12 15:20:41 +0300

    feat: Added Home section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,40 @@
+import Image from 'next/image';
+
 import Navbar from '@/components/Navbar';
+import Heading from '@/components/Heading';
+
+import homeLogo from '@/public/home-logo.png';
+import eagle from '@/public/eagle.png';
 
 export default function Home() {
   return (
     <main className=" text-white relative ">
       <Navbar />
+
+      <section className="flex flex-col justify-center items-center container py-14">
+        <Image
+          src={homeLogo}
+          alt="home logo"
+          className="w-1/2 lg:w-4/12 mb-2 "
+        />
+        <p className="text-base md:text-lg lg:text-xl text-white font-medium mb-10">
+          Ultra Targeted Leads & Sales, Let's Prey
+        </p>
+        <Image
+          src={eagle}
+          alt="home logo"
+          className="w-1/2 md:w-1/3 lg:w-1/4"
+        />
+
+        <Heading level={2} className="font-bold" color="blue">
+          Huge Updates On-The-Way
+        </Heading>
+        <li className="font-medium text-lg text-center">
+          xpanding in existing markets: Canada, UK, and US and promoting our
+          upgraded leads services there
+        </li>
+        <li>Entering new targeted markets: UAE</li>
+      </section>
     </main>
   );
 }
---
+++ components/Heading.jsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+import React from 'react';
+
+const Heading = ({
+  level = 1,
+  children,
+  color = 'text-white',
+  className = '',
+}) => {
+  const Tag = `h${level}`;
+
+  const colorStyles = {
+    blue: ' text-[#027FCA]',
+    gradient:
+      'bg-clip-text text-transparent bg-gradient-to-br from-[#FF0000] via-[#027FCA] to-[#027FCA] ',
+  };
+
+  const baseStyles = 'text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold';
+
+  const combinedStyles = classNames(baseStyles, colorStyles[color], className);
+
+  return <Tag className={combinedStyles}>{children}</Tag>;
+};
+
+export default Heading;
