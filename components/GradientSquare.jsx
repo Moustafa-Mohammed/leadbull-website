e82d8c1504420c57457424ca864ede83d5f4f2b1
Mostafa Mohamed <thedevelopermostafa@gmail.com>
2024-08-16 15:25:31 +0300

    refactor: adjusted the square width and height

--- components/GradientSquare.jsx
+++ components/GradientSquare.jsx
@@ -2,9 +2,9 @@ const GradientSquare = ({ className = '', children }) => {
   return (
     <div className={`relative -rotate-45  ${className}`}>
       <div
-        className={` w-[4.75rem] h-[4.75rem] sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 bg-gradient-to-r-square rounded-md grid place-content-center shadow-lg`}
+        className={` w-[4.5rem] h-[4.5rem] sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 bg-gradient-to-r-square rounded-md grid place-content-center shadow-lg`}
       >
-        <h6 className="text-white text-xs font-medium rotate-45 text-center">
+        <h6 className="text-white text-xs  font-normal sm:font-medium rotate-45 text-center">
           {children}
         </h6>
       </div>
